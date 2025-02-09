7e26d3c31f99cbee08d33758a7ca790f9f05873b
renancvitor <renan.vitor.cm@gmail.com>
2025-02-09 11:28:29 -0300

    Teste git new project

---
+++ src/tw/algoritmos/Main.java
@@ -0,0 +1,12 @@
+package tw.algoritmos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[5];
+        numeros[0] = 30;
+
+        System.out.println(numeros[0]);
+        System.out.println("Hello world");
+    }
+}
