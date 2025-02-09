bafc1f0a00b34fdc5c83369f3cf6c896217ac3f7
renancvitor <renan.vitor.cm@gmail.com>
2025-02-09 12:01:49 -0300

    Laço for

--- src/tw/algoritmos/Main.java
+++ src/tw/algoritmos/Main.java
@@ -1,12 +1,28 @@
 package tw.algoritmos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         int[] numeros = new int[5];
         numeros[0] = 30;
 
-        System.out.println(numeros[0]);
         System.out.println("Hello world");
+        System.out.println("Digite um número: ");
+
+        for (int i = 0; i < numeros.length; i++) {
+            int numero = scanner.nextInt();
+            numeros[i] = numero;
+        }
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        scanner.close();
     }
 }
